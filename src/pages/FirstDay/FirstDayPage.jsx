58b916507a62227af309ac33d49f9d0b87919656
Dan-FC <danielclausen12@gmail.com>
2025-04-04 09:14:55 -0700

    staging branch

--- src/pages/FirstDay/FirstDayPage.jsx
+++ src/pages/FirstDay/FirstDayPage.jsx
@@ -9,7 +9,7 @@ export default function FirstDayPage() {
 
       <div>
         <h1>Día 1</h1>
-        <p>Bienvenido al primer día de la competencia.</p>
+        <p>Bienvenido al primer día de la competencia:</p>
         <p>¡Prepárate para la emoción!</p>
       </div>
     </>
