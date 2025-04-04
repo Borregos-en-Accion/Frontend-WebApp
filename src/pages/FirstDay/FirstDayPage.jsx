52cdd76e9dc221a935613fe0271b47d67a629971
Dan-FC <danielclausen12@gmail.com>
2025-04-04 09:20:51 -0700

    div error fixed

--- src/pages/FirstDay/FirstDayPage.jsx
+++ src/pages/FirstDay/FirstDayPage.jsx
@@ -6,24 +6,26 @@ import styles from "./FirstDay.module.css";
 export default function FirstDayPage() {
   return (
     <>
-      <Navbar />
-      <div className={styles.container}>
-        {/* Título centrado */}
-        <h1 className={styles.title}>Día 1: Fútbol</h1>
+    <Navbar />
+    <div className={styles.container}>
+      {/* Título centrado */}
+      <h1 className={styles.title}>Día 1: Fútbol</h1>
 
-        {/* Subtítulo centrado */}
-        <div className={styles.subtitle}>
-          <h2 className={styles.category}>Categoría: Profesional Rama: Varonil</h2>
-          <p>Bienvenidos al primer día de Borregos en Acción.</p>
-          <p className={styles.text}>
-            Selecciona entre los diferentes filtros para poder ver entre Deportes, Categoría, Rama, etc.
-          </p>
-        </div>
+      {/* Subtítulo centrado */}
+      <div className={styles.subtitle}>
+        <h2 className={styles.category}>Categoría: Profesional Rama: Varonil</h2>
+        <p>Bienvenidos al primer día de Borregos en Acción.</p>
+        <p className={styles.text}>
+          Selecciona entre los diferentes filtros para poder ver entre Deportes, Categoría, Rama, etc.
+        </p>
+      </div>
 
-        {/* Contenedor para ajustar la tabla */}
-        <div className={styles.tableContainer}>
-          <Table />
-        </div>
+      {/* Contenedor para ajustar la tabla */}
+      <div className={styles.tableContainer}>
+        <Table />
+      </div>
+    </div>
     </>
+ 
   );
-}
+};
